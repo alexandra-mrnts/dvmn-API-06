9d641f9418224dcfdda3db8ac3a08df658827ce7
alexandra-mrnts <143014934+alexandra-mrnts@users.noreply.github.com>
2025-03-11 18:36:08 +0300

    Remove unnecessary functionality from download_image()

--- download_web_image.py
+++ download_web_image.py
@@ -1,10 +1,8 @@
 import requests
-from pathlib import Path
 
 
 def download_image(url, filepath, query_params=None):
     response = requests.get(url, query_params)
     response.raise_for_status()
-    Path(filepath).parent.mkdir(parents=True, exist_ok=True)
     with open(filepath, 'wb') as file:
         file.write(response.content)
